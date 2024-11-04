0cb3c41109cb5320ee92e3d2d1f89dc7dc0cdb07
Suzanne <suzannetyers@gmail.com>
2024-11-04 19:11:20 +0000

    login with amazon

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,19 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
-  },
+    externalProviders: {
+      loginWithAmazon: {
+        clientId: secret('amzn1.application-oa2-client.3ea04696df8440381a1774c5d751521'),
+        clientSecret: secret('amzn1.oa2-cs.v1.79b4b56049ce083a8c57a74ae9d4c047179d4976c996199562660248d032e985'),       
+        attributeMapping: {
+          email: 'email'
+        }
+      },
+      callbackUrls: [
+        'http://localhost:5173/',
+        'https://main.dwcj519lsme07.amplifyapp.com/'
+      ],
+      logoutUrls: ['http://localhost:5173/', 'https://main.dwcj519lsme07.amplifyapp.com/'],
+    }
+  }
 });
--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(  
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator hideSignUp socialProviders={['amazon']} loginMechanisms={['email']} signUpAttributes={['email']}>
       <App />
     </Authenticator>
   </React.StrictMode>
