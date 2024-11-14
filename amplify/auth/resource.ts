2c7a8a10288ee98706d8ec490aad5d0463354fce
Suzanne <suzannetyers@gmail.com>
2024-11-14 12:58:46 +0000

    saml provider

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,16 +8,12 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
     externalProviders: {
-      google: {
-        clientId: secret('GOOGLE_CLIENTID'),
-        clientSecret: secret('GOOGLE_SECRET')
-      },
-      loginWithAmazon: {
-        clientId: secret('AMAZON_CLIENTID'),
-        clientSecret: secret('AMAZON_SECRET'),       
-        attributeMapping: {
-          email: 'email'
-        }
+      saml: {
+        name: 'EverywareEntra',
+        metadata: {
+          metadataContent: 'https://login.microsoftonline.com/530083c0-e368-4614-8d13-cb3ac9073740/federationmetadata/2007-06/federationmetadata.xml?appid=9306f21f-c48b-4ae7-a2df-35b5adcd879b', // or content of the metadata file
+          metadataType: 'URL'
+        },
       },
       callbackUrls: [
         'http://localhost:5173/',
