8e58860e677d3d7991adfe8857d6f5c9a79602f5
Suzanne <suzannetyers@gmail.com>
2024-12-16 18:14:42 +0000

    mapview

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,27 @@
 import './init.js'
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-import '@aws-amplify/ui-react/styles.css';
-import App from './App.tsx';
+import { Amplify } from 'aws-amplify';
+import { MapView } from '@aws-amplify/ui-react-geo';
 
-Amplify.configure(outputs);
+import '@aws-amplify/ui-react-geo/styles.css';
+
+import awsExports from "../amplify_outputs.json";
+
+Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(  
   <React.StrictMode>
-      <App />
+      <MapView
+      initialViewState={{
+        latitude: 37.8,
+        longitude: -122.4,
+        zoom: 14,
+      }}
+    />
   </React.StrictMode>
 );
+
+//init js to fix vite global err
+//node polyfills install
+//leave create map in backend.ts
